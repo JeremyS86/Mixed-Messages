45ac6608a12d07441393b11ae8f69cb835da1873
Jeremy Sluggett <jeremy3215@yahoo.com.au>
2020-12-05 19:17:20 +1100

    final javascript program finished

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,15 +1,22 @@
 randomJokes = [
-    'I used to be addicted to the hokey pokey… but then I turned myself around.', 
-    'What\’s the most terrifying word in nuclear physics?...“Oops!”',
-    'I was wondering why the ball kept getting bigger and bigger...And then it hit me.',
-    'I got fired from my job at the bank today...An old lady came in and asked me to check her balance, so I pushed her over.'
+    ['I used to be addicted to the hokey pokey...', 'but then I turned myself around.'], 
+    ['What\’s the most terrifying word in nuclear physics?...', '“Oops!”'],
+    ['I was wondering why the ball kept getting bigger and bigger...', 'And then it hit me.'],
+    ['I got fired from my job at the bank today...', 'An old lady came in and asked me to check her balance, so I pushed her over.']
 ]
+
+const punchLine = () => {
+    const punchLine = console.log(randomJokes[randomNum][1]);
+    return punchLine;
+}
+
 
 const randomNum = Math.floor(Math.random() * 4);
 
 
 const message = randNum => {
-    console.log(randomJokes[randNum]);
+    console.log(randomJokes[randNum][0]);
+    setTimeout(punchLine, 3000);
 }
 
 message(randomNum);
